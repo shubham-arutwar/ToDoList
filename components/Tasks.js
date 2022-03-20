4e58b955dd7b7225bc5c8819d1f4cdfaea99bb56
shubham-arutwar <shubhamarutwar23@gmail.com>
2022-03-20 23:44:28 +0530

    added some front end, looks presentable

--- components/Tasks.js
+++ components/Tasks.js
@@ -5,7 +5,7 @@ const Task = (props) => {
     return (
         <View style={styles.item}>
             <View style={styles.itemLeft}>
-                <TouchableOpacity style={styles.square}></TouchableOpacity>
+                <View style={styles.square}></View>
                 <Text style={styles.itemText}>{props.text}</Text>
             </View>
             <View style={styles.circle}></View>
@@ -17,10 +17,25 @@ const styles = StyleSheet.create({
     item: {
         backgroundColor: '#fff',
         padding: 15,
-        borderRadius: 10
+        borderRadius: 10,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginBottom: 20,
     },
-    itemLeft: {},
-    square: {},
+    itemLeft: {
+        flexDirection: "row",
+        alignItems: 'center',
+        flexWrap: 'wrap',
+    },
+    square: {
+        width: 24,
+        height: 24,
+        backgroundColor: '#55BCF6',
+        opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15,
+    },
     text: {},
     circle: {},
 
